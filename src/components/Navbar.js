bc7345ca97bfc23e410e6cef511e50e45391e9d3
mdnelles <mxnelles@gmail.com>
2020-03-20 18:12:10 -0400

    modified:   src/AppWrapper.js 	modified:   src/components/AddCodeBase.js 	modified:   src/components/Navbar.js 	modified:   src/components/Search.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
-import React, { Fragment, useState} from "react";
+import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { getTitles } from './SearchFunctions';
-import localForage from 'localforage'
+import localForage from 'localforage';
 
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import Button from '@material-ui/core/Button';
@@ -12,181 +12,152 @@ import MenuIcon from '@material-ui/icons/Menu';
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
-import {
-  withStyles,
-  makeStyles,
-} from '@material-ui/core/styles';
-//import InputBase from '@material-ui/core/InputBase';
-//import InputLabel from '@material-ui/core/InputLabel';
+import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 
-import { logout } from "./UserFunctions"
+import { logout } from './UserFunctions';
 
 export const Navbar = () => {
-
-  const [ anchorEl, setAnchorEl ] = React.useState(null);
-  const [ token, setToken] = React.useState('no token');
-  const [ afDone, setAfDone] = React.useState(false);
-  const [ searchTitles, setSearchTitle ] = React.useState([]);
-  const [ searchREST, setSearchREST ] = React.useState('')
-  const [ query, setQuery ] = React.useState('');
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [afDone, setAfDone] = React.useState(false);
+  const [searchTitles, setSearchTitle] = React.useState([]);
+  const [query, setQuery] = React.useState('');
   const history = useHistory();
-  const handleClick = event => {
+  const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
 
-  const popAuto = () => {
-    console.log('popAuto');
-  }
-
-  const handleClose = e => {
+  const handleClose = (e) => {
     window.top.location = '/' + e.target.id;
     setAnchorEl(null);
   };
 
-  React.useEffect (() => {
+  React.useEffect(() => {
     //localForage.getItem('token', function(err, theToken) {
-      if(afDone === false){
-
-        localForage.getItem('token').then(function(theToken) {
-          setToken(theToken);
-          if(searchTitles.length < 2){
-              getTitles(theToken).then( data => {
-                setSearchTitle(data);
-                setAfDone(true);
-              })
-          }
-        })
-      }
-  },[])
+    if (afDone === false) {
+      localForage.getItem('token').then(function(theToken) {
+        if (searchTitles.length < 2) {
+          getTitles(theToken).then((data) => {
+            setSearchTitle(data);
+            setAfDone(true);
+          });
+        }
+      });
+    }
+  }, [afDone]);
 
   /////////////////////////////////////// material ui theming
-  const autoCompVar = "Example1, Example2"
 
-  const useStyles = makeStyles(theme => ({
+  const useStyles = makeStyles((theme) => ({
     root: {
       display: 'flex',
-      flexWrap: 'wrap',
+      flexWrap: 'wrap'
     },
     margin: {
-      margin: theme.spacing(1),
-    },
+      margin: theme.spacing(1)
+    }
   }));
 
   const classes = useStyles();
 
-  const CssTextField = withStyles({
-    root: {
-      '& label': {
-        color:'#639fff'
-      },
-      '& borderBottomColor': '#639fff',
-      '& label.Mui-focused': {
-        color: 'white'
-      },
-      '& .MuiInput-underline:after': {
-        borderBottomColor: 'white',
-      },
-    },
-  })(TextField);
   /////////////////////////////////////// end material ui theming
 
-
-
-  const addSelected = event =>{
-
+  const addSelected = (event) => {
     setTimeout(() => {
       var temp = document.getElementById('autocomplete').value;
       console.log(temp);
       setQuery(encodeURI(temp));
     }, 300); // need slight delay so DOM can update
-  }
+  };
 
-  const handleKeyDown = e  => {
+  const handleKeyDown = (e) => {
     if (e.key === 'Enter') {
       console.log('moving to search with query:  /search/' + query);
       history.push({
-        pathname: '/search/' +query + '/'
-      })
+        pathname: '/search/' + query + '/'
+      });
     }
-  }
+  };
 
- 
   return (
-    <div className="navAppBar">
-
+    <div className='navAppBar'>
       <Grid container spacing={3}>
-
         <Grid item xs={1} sm={1}>
           <div>
-            <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-              <MenuIcon style={{fill: "white"}} fontSize="large"  />
+            <Button
+              aria-controls='simple-menu'
+              aria-haspopup='true'
+              onClick={handleClick}
+            >
+              <MenuIcon style={{ fill: 'white' }} fontSize='large' />
             </Button>
             <Menu
-              id="simple-menu"
+              id='simple-menu'
               anchorEl={anchorEl}
               keepMounted
               open={Boolean(anchorEl)}
               onClose={handleClose}
             >
-              <MenuItem onClick={handleClose} id="add">Add Code</MenuItem>
-              <MenuItem onClick={handleClose} id="search">Search Code</MenuItem>
-              <MenuItem onClick={handleClose} id="users">Users</MenuItem>
-              <MenuItem onClick={handleClose} id="pma">PhpMyAdmin</MenuItem>
-              <MenuItem onClick={logout} >Logout</MenuItem>
+              <MenuItem onClick={handleClose} id='add'>
+                Add Code
+              </MenuItem>
+              <MenuItem onClick={handleClose} id='search'>
+                Search Code
+              </MenuItem>
+              <MenuItem onClick={handleClose} id='users'>
+                Users
+              </MenuItem>
+              <MenuItem onClick={handleClose} id='pma'>
+                PhpMyAdmin
+              </MenuItem>
+              <MenuItem onClick={logout}>Logout</MenuItem>
             </Menu>
           </div>
         </Grid>
 
         <Grid item xs={12} sm={10}>
-                
           <form className={classes.root} noValidate>
-          <Autocomplete
-              id="autocomplete"
-              size="small"
+            <Autocomplete
+              id='autocomplete'
+              size='small'
               autoFocus={true}
               options={searchTitles}
-              onChange={ event => addSelected(event)}
-              getOptionLabel={option => option.title}
-              style={{ 
-                width:600,
-                color: "white",
-                marginTop:-20
+              onChange={(event) => addSelected(event)}
+              getOptionLabel={(option) => option.title}
+              style={{
+                width: 600,
+                color: 'white',
+                marginTop: -20
               }}
-              renderInput={params => <TextField {...params} 
-                                        label="Search Codebase"  
-                                        id="queryField"
-                                        margin="normal"
-                                        onChange={ event => setQuery(encodeURI(event.target.value))}
-                                        onKeyDown={handleKeyDown}
-                                      />}
-          />
-
-          </form>                
-
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label='Search Codebase'
+                  id='queryField'
+                  margin='normal'
+                  onChange={(event) => setQuery(encodeURI(event.target.value))}
+                  onKeyDown={handleKeyDown}
+                />
+              )}
+            />
+          </form>
         </Grid>
 
         <Grid item xs={1} sm={1}>
-        
-          <AddCircleOutlineIcon className="pointer" 
-                            style={{fill: "white", marginTop:10}} 
-                            onClick={ ()=> window.location.href='/add'} 
-                            fontSize="large"
-                            />
-          <AccountCircleIcon className="pointer" 
-                            style={{fill: "white", marginTop:10}} 
-                            onClick={logout} 
-                            fontSize="large"
-                            />
+          <AddCircleOutlineIcon
+            className='pointer'
+            style={{ fill: 'white', marginTop: 10 }}
+            onClick={() => (window.location.href = '/add')}
+            fontSize='large'
+          />
+          <AccountCircleIcon
+            className='pointer'
+            style={{ fill: 'white', marginTop: 10 }}
+            onClick={logout}
+            fontSize='large'
+          />
         </Grid>
-
       </Grid>
-
-
-
     </div>
   );
-}
-
-
-
+};
